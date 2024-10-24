4a989b7bed2bbe3ab39f96346572e395d768d217
AbdurRob <abdurrobr34l@gmail.com>
2024-10-24 22:24:14 +0600

    go-to-top button added, minor changes done in css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-// HAMBURGER MENU AND ICON START 
+// GO-TO-TOP BUTTON START
+window.onscroll = function () {
+  var button = document.getElementById('goToTop');
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    button.style.display = 'block'; // Show button
+  } else {
+    button.style.display = 'none'; // Hide button
+  }
+};
+// GO-TO-TOP BUTTON END  ---------
+
+// HAMBURGER MENU AND ICON START  ---------
 const hamburgerIcon = document.querySelector('.header__hamburgermenu-icon .fa-bars-staggered');
 const crossIcon = document.querySelector('.header__hamburgermenu-icon .fa-xmark');
 const menu = document.querySelector('.hamburgermenu');
@@ -42,7 +53,7 @@ hireMeButton.addEventListener('click', closeMenu);
 // HAMBURGER MENU AND ICON END  ---------
 
 // TYPEWRITER EFFECT FOR ABOUT ME SECTION START ---------
-var TxtType = function(el, toRotate, period) {
+var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
@@ -52,17 +63,17 @@ var TxtType = function(el, toRotate, period) {
   this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
+TxtType.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
   if (this.isDeleting) {
-  this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
   var delta = 200 - Math.random() * 100;
@@ -70,27 +81,27 @@ TxtType.prototype.tick = function() {
   if (this.isDeleting) { delta /= 2; }
 
   if (!this.isDeleting && this.txt === fullTxt) {
-  delta = this.period;
-  this.isDeleting = true;
-} else if (this.isDeleting && this.txt === '') {
-  this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
   }
-  
-  setTimeout(function() {
+
+  setTimeout(function () {
     that.tick();
   }, delta);
 };
 
-window.onload = function() {
+window.onload = function () {
   var elements = document.getElementsByClassName('typewrite');
-  for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
   }
   // INJECT CSS
   var css = document.createElement("style");
