7f5480532731cd4f36df63f6e3280160d77e06c0
AbdurRob <abdurrobr34l@gmail.com>
2025-01-27 01:06:20 +0600

    Finally updated my portfolio contents

--- js/year.js
+++ js/year.js
@@ -1,7 +1,1 @@
-// FOOTER START ---------
-// Wait for the DOM to fully load
-document.addEventListener("DOMContentLoaded", function() {
-  // Set the copyright year dynamically
-  document.getElementById("year").textContent = new Date().getFullYear();
-});
-// FOOFTER END ---------
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("year").textContent=(new Date).getFullYear()});
